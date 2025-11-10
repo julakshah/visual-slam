fcc3811aeaf49ffb0312938b80163247248ed0e0
julakshah <julakshah@gmail.com>
2025-11-09 23:10:58 -0500

    working feature matching

---
+++ views.py
@@ -0,0 +1,49 @@
+from queue import Queue
+import numpy as np
+import cv2 as cv
+
+import sdl2
+import sdl2.ext
+
+
+class Vid:
+    def __init__(self, disp_queue):
+        # init ORB algorithm
+        self.orb = cv.ORB_create(nfeatures=5000)
+        # init Brute Force matcher for
+        self.matcher = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
+        self.disp_q = disp_queue  # queue to send results to
+
+    def feature_matching(self, im1, im2):
+        # args are InputArray image, InputArray mask
+        # output is keypoints, descriptors
+        k1, d1 = self.orb.detectAndCompute(im1, None)
+        k2, d2 = self.orb.detectAndCompute(im2, None)
+
+        matches = self.matcher.match(d1, d2)
+        matches = sorted(matches, key=lambda x: x.distance)
+
+        im3 = cv.drawMatches(
+            im1,
+            k1,
+            im2,
+            k2,
+            matches[:10],
+            None,
+            flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS,
+        )
+        # self.disp_q.put(matches)
+        return im3
+
+
+class Map:
+    def __init__(self):
+        self.frames = []
+        self.points = []
+
+
+class Frame:
+    def __init__(self, mapp, img):
+        self.id = len(mapp.frames)  # give self the last id
+        self.img = img
+        mapp.frames.append(self)  # append itself to the map
